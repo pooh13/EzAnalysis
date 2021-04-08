4403ca9b2880ce03cfb9d3d7ceffb00110406604
kellyhu0109 <kellyhu0109@gmail.com>
2021-04-08 14:53:18 +0800

    test

--- EmotionDiary/AI_analyze/views.py
+++ EmotionDiary/AI_analyze/views.py
@@ -20,6 +20,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
+
 @csrf_exempt
 def callback(request):
     if request.method == 'POST':
