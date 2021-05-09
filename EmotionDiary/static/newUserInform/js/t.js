53d99a42321490344654a9997309ae2058bb38a7
kellyhu0109 <kellyhu0109@gmail.com>
2021-05-09 10:34:00 +0800

    nothing update

---
+++ EmotionDiary/static/newUserInform/js/t.js
@@ -0,0 +1,3 @@
+function myMsg(){
+    alert("儲存成功");
+}
---
+++ EmotionDiary/static/newUserInform/js/test.js
@@ -0,0 +1,33 @@
+//function test(obj){
+//    var arr = document.getElementsByTagName('button');
+//    var g_id = this.id;
+//    alert(arr);
+//    alert(g_id);
+//}
+
+
+//var g_id;
+
+//window.onclick = function genderselect(){
+//    var arr = document.getElementsByTagName('button');
+//    for(var i = 0;i<arr.length;i++){
+//        arr[i].onclick = function(){
+//            //this是当前激活的按钮，在这里可以写对应的操作
+//            if(this.className == 'btn1'){
+//                this.className = 'btn2';
+//                g_id = this.id;
+//                var btn = document.getElementsByClassName('btn2');
+//                for(var j=0;j<btn.length;j++){
+//                    if(btn[j].id!=g_id){
+//                        btn[j].className = 'btn1';
+//                    }
+//                }
+//            }
+//        }
+//    }
+//    alert(g_id);
+//}
+
+function myMsg(){
+    alert("儲存成功");
+}
