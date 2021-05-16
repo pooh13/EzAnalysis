ed6938a1f2fa16ee07526912454680eadeb3fea7
Eason Fang <10456009@ntub.edu.tw>
2021-05-17 01:19:30 +0800

    update file "main.py"_editing

--- textEmotionalAnalysis/main.py
+++ textEmotionalAnalysis/main.py
@@ -1,6 +1,47 @@
-import analysisFunction
+import MySQL_DB_Link
+import MyJieba_hant
+import cursorToList
+import toList
+
 
 def textEmotionalAnalysis():
-    pass
+    queryList = (b'select ? from ?')
+    # print(queryList)
+    args = (b'test.text', b'localtest.test')
+    a = cursorToList.cursorToList(MySQL_DB_Link.MySQL_DB_Link(MySQL_DB_Link.MySQLConnectInfoStr[0],
+                                                     MySQL_DB_Link.MySQLConnectInfoStr[1],
+                                                     MySQL_DB_Link.MySQLConnectInfoStr[2],
+                                                     MySQL_DB_Link.MySQLConnectInfoStr[3],
+                                                     MySQL_DB_Link.MySQLConnectInfoStr[4],).query(queryList, args))
+    print(a)
+
+    # b = cursorToList.cursorToList(a)
+    # print(b)
+
+    # for contextRow in cursorToList.cursorToList(queryList):
+    #     jiebaResult = str(MyJieba_hant.MyJieba_hant(str(contextRow)))
+    #     # print(type(jiebaResult))
+    #     print(jiebaResult)
+    #     word = list.append(context)
+    #     print(word)
+    # print(type(context))
+    # word = MyJieba_hant.MyJieba_hant(context)
+    # print(word)
+
+    # print(type(queryList))
+    # print(type(word))
+    #
+    # print(queryList)
+    # print(word)
+
 
 textEmotionalAnalysis()
+
+# word = MyJieba_hant.MyJieba_hant(context)
+# print(word)
+
+
+# def textEmotionalAnalysis():
+#     pass
+#
+# textEmotionalAnalysis()
