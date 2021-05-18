a2878201ba141615f2bd3678b4529ef1c9d806fa
Eason Fang <10456009@ntub.edu.tw>
2021-05-19 00:40:10 +0800

    update file "main.py"_editing

--- textEmotionalAnalysis/main.py
+++ textEmotionalAnalysis/main.py
@@ -1,7 +1,9 @@
-import MySQL_DB_Link
+import os
 import MyJieba_hant
 import cursorToList
-import toList
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
 def textEmotionalAnalysis():
@@ -12,30 +14,19 @@ def textEmotionalAnalysis():
     # queryList = (f'select test.text from localtest.test where id = ' + line_id)
 
     # args = (b'test.text', b'localtest.test')
-    a = cursorToList.cursorToList(queryList)
-    print(a)
 
-    # for contextRow in cursorToList.cursorToList(queryList):
-    #     jiebaResult = str(MyJieba_hant.MyJieba_hant(str(contextRow)))
-    #     # print(type(jiebaResult))
-    #     print(jiebaResult)
-    #     word = list.append(context)
-    #     print(word)
-    # print(type(context))
-    # word = MyJieba_hant.MyJieba_hant(context)
-    # print(word)
+    # jieba
+    for contextRow in cursorToList.cursorToList(queryList):
+        JiebaResult = str(MyJieba_hant.MyJieba_hant(str(contextRow)))
 
-    # print(type(queryList))
-    # print(type(word))
-    #
-    # print(queryList)
-    # print(word)
+        # # print(type(JiebaResult))
+        print(JiebaResult)
+        # return JiebaResult
+
+
 
 
 textEmotionalAnalysis()
-
-# word = MyJieba_hant.MyJieba_hant(context)
-# print(word)
 
 # main.py original
 # def textEmotionalAnalysis():
