1dcca6d3bfd7de37175575fbdbd09c2f5ef1ff06
Eason Fang <pcdanny0819@hotmail.com>
2021-05-19 18:50:39 +0800

    upadte Project "textEmotionalAnalysis"
    update file "main.py", "one_hotEncoding.py", "requirement.txt" & change file name "one-hotEncoding.py"

--- textEmotionalAnalysis/main.py
+++ textEmotionalAnalysis/main.py
@@ -1,6 +1,7 @@
 import os
 import MyJieba_hant
 import cursorToList
+import one_hotEncoding
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -16,12 +17,17 @@ def textEmotionalAnalysis():
     # args = (b'test.text', b'localtest.test')
 
     # jieba
-    for contextRow in cursorToList.cursorToList(queryList):
-        JiebaResult = str(MyJieba_hant.MyJieba_hant(str(contextRow)))
+    # for contextRow in cursorToList.cursorToList(queryList):
+    #     JiebaResult = str(MyJieba_hant.MyJieba_hant(str(contextRow)))
 
-        # # print(type(JiebaResult))
-        print(JiebaResult)
-        # return JiebaResult
+    def JiebaResult():
+        return[str(MyJieba_hant.MyJieba_hant(str(contextRow))) for contextRow in cursorToList.cursorToList(queryList)]
+
+    print(type(JiebaResult()))
+    print(JiebaResult())
+    print(JiebaResult()[2])
+
+    # one_hotEncoding.oneHotEncoding(JiebaResult())
 
 
 
--- textEmotionalAnalysis/one_hotEncoding.py
+++ textEmotionalAnalysis/one_hotEncoding.py
@@ -1,12 +1,15 @@
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 
-def oneHotEncoding():
+def oneHotEncoding(bow):
+
+    comment_bow = dict()
+
     bow_array = np.array(list(bow)).reshape(-1, 1)
     onehot = OneHotEncoder().fit(bow_array)
 
-    X = np.ndarray((len(df2),len(bow)))
-    for i in range(len(df2)):
+    X = np.ndarray((len(bow),len(bow)))
+    for i in range(len(bow)):
         # 少數僅含空白、標點符號之類而被刪除完內容的評論
         if(len(comment_bow[i])==0):
             X[i] = np.zeros((len(bow)))
