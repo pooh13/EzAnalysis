9f14e87d281ba306eee37529c341d8fedb53768c
Eason Fang <pcdanny0819@hotmail.com>
2021-06-15 16:22:09 +0800

    update Project "textEmotionalAnalysis"
    check point "createFolder.py" can call and return & update file "createFolder.py"

--- textEmotionalAnalysis/createFolder.py
+++ textEmotionalAnalysis/createFolder.py
@@ -4,20 +4,29 @@ import os
 
 def createFolder(path):
 
-    if platform.system() == systemPlatform[0] \
-            or platform.system() == systemPlatform[1] \
-            or platform.system() == systemPlatform[2] \
-            or platform.system() == systemPlatform[3]:
-        path = (r'\Models\MLP_Model\\')
+    systemPlatform = ('windows', 'dos')
+
+    if platform.system() == systemPlatform[0].capitalize() \
+            or platform.system() == systemPlatform[0] \
+            or platform.system() == systemPlatform[1].title() \
+            or platform.system() == systemPlatform[1]:
+
+        if path.find('/') == 0:
+            path = path.replace('/', '\\')
+        else:
+            path = path
     else:
-        path = ('/Models/MLP_Model/')
+        if path.find('\\') == 0:
+            path = path.replace('\\', '/')
+        else:
+            path = path
 
     localtime = time.strftime('%Y%m%d_%H%M%S',time.localtime(time.time()))
 
     if not os.path.exists(os.getcwd()+path+localtime):
         createdPath = os.getcwd()+path+localtime
         os.makedirs(createdPath, mode=0o777)
-        print('Folder Create Finish！')
+        print('Folder Create Finish！\nFolder PATH > "'+createdPath+'"')
     else:
         pass
 
